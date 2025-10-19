db321afcb5f6978c24c0bf39f6c29142d3be074a
Javier143322 <huhdnuhdl@gmail.com>
2025-10-19 14:52:29 -0500

    Actualizar fxmanifest.lua

--- tactical_belt/fxmanifest.lua
+++ tactical_belt/fxmanifest.lua
@@ -1,1 +1,27 @@
 
+fx_version 'cerulean'
+game 'gta5'
+
+author 'TuNombre'
+description 'Sistema de Cinturera Táctica para ESX'
+version '1.0.0'
+
+client_scripts {
+    'config.lua',
+    'client/main.lua',
+    'client/item_usage.lua'
+}
+
+server_scripts {
+    'server/main.lua'
+}
+
+shared_scripts {
+    '@es_extended/imports.lua',  -- Si usas ESX Legacy
+    -- '@es_extended/locale.lua', -- Descomenta si usas locales
+    -- 'locales/*.lua'            -- Descomenta si usas locales
+}
+
+dependencies {
+    'es_extended'
+}
